26c56241f7a13918ff2ba4fdbb2463116bb7951c
ariprasathsakthivel <67471289+ariprasathsakthivel@users.noreply.github.com>
2021-08-01 10:57:26 +0530

    [Ariprasath] added: Welcome note to the Addressbook program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookUsingCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address book program");
+        }
+    }
+}
